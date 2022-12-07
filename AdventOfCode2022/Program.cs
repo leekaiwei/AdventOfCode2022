d431e9c9bb7ae1d56fac886a73b91846482ecff1
Kevin Lee <kevinlee1@corelogic.com>
2022-12-07 22:38:22 +0000

    Day 1

---
+++ AdventOfCode2022/Program.cs
@@ -0,0 +1,51 @@
+﻿var lines = File.ReadAllLines("H:\\Repositories\\AdventOfCode2022\\AdventOfCode2022\\input.txt");
+var maximumTotal = 0;
+var currentTotal = 0;
+
+// part 1
+foreach (var line in lines)
+{
+    var isNumber = int.TryParse(line, out var lineTotal);
+    if (isNumber)
+    {
+        currentTotal+= lineTotal;
+    }
+    else
+    {
+        if (currentTotal > maximumTotal)
+        {
+            maximumTotal = currentTotal;
+        }
+
+        currentTotal = 0;
+    }
+}
+
+Console.WriteLine(maximumTotal);
+
+// part 2
+currentTotal = 0;
+var totals = new List<int>();
+foreach (var line in lines)
+{
+    var isNumber = int.TryParse(line, out var lineTotal);
+    if (isNumber)
+    {
+        currentTotal += lineTotal;
+    }
+    else
+    {
+        totals.Add(currentTotal);
+
+        currentTotal = 0;
+    }
+}
+
+maximumTotal = 0;
+var orderedTotals = totals.OrderByDescending(total => total);
+for (var i = 0; i <= 2; i++)
+{
+    maximumTotal += orderedTotals.ElementAt(i);
+}
+
+Console.WriteLine(maximumTotal);
