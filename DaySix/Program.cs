cd69f7ce6545817e58339b6645b3054f26d34f35
Kevin Lee <kevinlee1@corelogic.com>
2022-12-08 13:46:30 +0000

    Day 6

---
+++ DaySix/Program.cs
@@ -0,0 +1,37 @@
+﻿var input = await File.ReadAllTextAsync("H:\\Repositories\\AdventOfCode2022\\DaySix\\input.txt");
+
+// part 1
+var index = 4;
+var characters = input[..4];
+Output(index, characters, input);
+
+index = 14;
+characters = input[..14];
+Output(index, characters, input);
+
+void Output(int index, string characters, string input)
+{
+    while (!IsMarker(characters))
+    {
+        characters = characters[1..];
+        characters += input[index];
+        index++;
+    }
+
+    Console.WriteLine(index);
+}
+
+
+static bool IsMarker(string input)
+{
+    var set = new HashSet<char>();
+    for (int i = 0; i < input.Length; i++)
+    {
+        if (!set.Add(input[i]))
+        {
+            return false;
+        }
+    }
+        
+    return true;
+}
