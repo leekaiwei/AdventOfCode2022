2fd5f9163410f25abe08a50c4983a6bedb0f36ce
Kevin Lee <kevinlee1@corelogic.com>
2022-12-14 14:08:23 +0000

    Day 14 Part 2

--- DayFourteen/Program.cs
+++ DayFourteen/Program.cs
@@ -1,89 +1,223 @@
 ﻿var lines = await File.ReadAllLinesAsync("H:\\Repositories\\AdventOfCode2022\\DayFourteen\\input.txt");
-var grid = new HashSet<Point>();
 const int sandStartingX = 500;
 
-foreach (var line in lines)
-{
-    var rocks = line.Split(" -> ");
-    for (var i = 0; i < rocks.Length - 1; i++)
-    {
-        var fromRock = new Point(rocks[i], Status.Rock);
-        var toRock = new Point(rocks[i + 1], Status.Rock);
-
-        var instruction = fromRock.InstructionTo(toRock);
-        if (instruction is null)
-        {
-            continue;
-        }
-
-        if (instruction.Axis == Axis.X)
-        {
-            for (var x = fromRock.X; x != instruction.PointToStop; x += instruction.Direction)
-            {
-                grid = Add(grid, new Point(x, fromRock.Y, Status.Rock));
-            }
-        }
-
-        if (instruction.Axis == Axis.Y)
-        {
-            for (var y = fromRock.Y; y != instruction.PointToStop; y += instruction.Direction)
-            {
-                grid = Add(grid, new Point(fromRock.X, y, Status.Rock));
-            }
-        }
-    }
-}
-
-var isFilled = false;
-var mostBottomFilledY = grid.Max(p => p.Y);
 var numberOfSand = 0;
-while (!isFilled)
-{
-    Point sandRestingPoint;
-    var stack = new Stack<Point>();
-    var mostBottomFreePointYInX = grid.Where(p => p.X == sandStartingX).Min(p => p.Y) - 1;
-    var startPoint = new Point(sandStartingX, mostBottomFreePointYInX, Status.Sand);
-    stack.Push(startPoint);
-    while (stack.TryPop(out var point))
-    {
-        var nextY = point.Y + 1;
-        if (nextY > mostBottomFilledY)
-        {
-            isFilled = true;
-            break;
-        }
 
-        var pointBelow = grid.SingleOrDefault(p => p.X == point.X && p.Y == nextY);
-        var pointToTheLeftDown = grid.SingleOrDefault(p => p.X == point.X - 1 && p.Y == nextY);
-        var pointToTheRightDown = grid.SingleOrDefault(p => p.X == point.X + 1 && p.Y == nextY);
-        if (pointBelow is null)
-        {
-            stack.Push(new Point(point.X, nextY, Status.Sand));
-            continue;
-        }
-        else if (pointToTheLeftDown is null)
-        {
-            stack.Push(new Point(point.X - 1, nextY, Status.Sand));
-            continue;
-        }
-        else if (pointToTheRightDown is null)
-        {
-            stack.Push(new Point(point.X + 1, nextY, Status.Sand));
-            continue;
-        }
-
-        grid.Add(point);
-        numberOfSand++;
-        Console.WriteLine(numberOfSand);
-        break;
-    }
-}
+//numberOfSand = PartOne(lines);
+numberOfSand = PartTwo(lines);
 
 Console.WriteLine(numberOfSand);
 
+int PartOne(string[] lines)
+{
+    var grid = GetStartingGrid(lines);
+    var isFilled = false;
+    var mostBottomFilledY = grid.Max(p => p.Y);
+    var numberOfSand = 0;
+    while (!isFilled)
+    {
+        Point sandRestingPoint;
+        var stack = new Stack<Point>();
+        var mostBottomFreePointYInX = grid.Where(p => p.X == sandStartingX).Min(p => p.Y) - 1;
+        var startPoint = new Point(sandStartingX, mostBottomFreePointYInX, Status.Sand);
+        stack.Push(startPoint);
+        while (stack.TryPop(out var point))
+        {
+            var nextY = point.Y + 1;
+            if (nextY > mostBottomFilledY)
+            {
+                isFilled = true;
+                break;
+            }
+
+            var pointBelow = grid.SingleOrDefault(p => p.X == point.X && p.Y == nextY);
+            var pointToTheLeftDown = grid.SingleOrDefault(p => p.X == point.X - 1 && p.Y == nextY);
+            var pointToTheRightDown = grid.SingleOrDefault(p => p.X == point.X + 1 && p.Y == nextY);
+            if (pointBelow is null)
+            {
+                stack.Push(new Point(point.X, nextY, Status.Sand));
+                continue;
+            }
+            else if (pointToTheLeftDown is null)
+            {
+                stack.Push(new Point(point.X - 1, nextY, Status.Sand));
+                continue;
+            }
+            else if (pointToTheRightDown is null)
+            {
+                stack.Push(new Point(point.X + 1, nextY, Status.Sand));
+                continue;
+            }
+
+            grid.Add(point);
+            numberOfSand++;
+            Console.WriteLine(numberOfSand);
+            break;
+        }
+    }
+
+    return numberOfSand;
+}
+
+int PartTwo(string[] lines)
+{
+    var grid = GetStartingGrid(lines);
+    var mostBottomFilledY = grid.Max(p => p.Y);
+    var floorY = mostBottomFilledY + 2;
+    var numberOfSand = 0;
+    var isFilled = false;
+    while (!isFilled)
+    {
+        Point sandRestingPoint;
+        var stack = new Stack<Point>();
+        var mostBottomFreePointYInX = grid.Where(p => p.X == sandStartingX).Min(p => p.Y) - 1;
+        var startPoint = new Point(sandStartingX, mostBottomFreePointYInX, Status.Sand);
+        stack.Push(startPoint);
+        while (stack.TryPop(out var point))
+        {
+            //Console.Clear();
+            //for (var y = 0; y <= floorY; y++)
+            //{
+            //    for (var x = 450; x < 550; x++)
+            //    {
+            //        var printer = grid.FirstOrDefault(p => p.X == x && p.Y == y);
+            //        if (y == point.Y && x == point.X)
+            //        {
+            //            Console.Write('~');
+            //        }
+            //        else if (y == floorY)
+            //        {
+            //            Console.Write('#');
+            //        }
+            //        else if (printer == null)
+            //        {
+            //            Console.Write('.');
+            //        }
+            //        else if (printer.Status == Status.Rock)
+            //        {
+            //            Console.Write('#');
+            //        }
+            //        else if (printer.Status == Status.Sand)
+            //        {
+            //            Console.Write('O');
+            //        }
+            //    }
+
+            //    Console.WriteLine();
+            //}
+            if (point.X == sandStartingX && point.Y < 0)
+            {
+                isFilled = true;
+                break;
+            }
+            var nextY = point.Y + 1;
+            if (nextY == floorY)
+            {
+                grid.Add(point);
+                numberOfSand++;
+                Console.WriteLine(numberOfSand);
+                break;
+            }
+
+            var pointBelow = grid.SingleOrDefault(p => p.X == point.X && p.Y == nextY);
+            var pointToTheLeftDown = grid.SingleOrDefault(p => p.X == point.X - 1 && p.Y == nextY);
+            var pointToTheRightDown = grid.SingleOrDefault(p => p.X == point.X + 1 && p.Y == nextY);
+
+            if (pointBelow is null)
+            {
+                stack.Push(new Point(point.X, nextY, Status.Sand));
+                continue;
+            }
+            else if (pointToTheLeftDown is null)
+            {
+                stack.Push(new Point(point.X - 1, nextY, Status.Sand));
+                continue;
+            }
+            else if (pointToTheRightDown is null)
+            {
+                stack.Push(new Point(point.X + 1, nextY, Status.Sand));
+                continue;
+            }
+
+            grid.Add(point);
+            numberOfSand++;
+            Console.WriteLine(numberOfSand);
+            break;
+        }
+
+        //Console.Clear();
+        //for (var y = 0; y <= floorY; y++)
+        //{
+        //    for (var x = 450; x < 550; x++)
+        //    {
+        //        var printer = grid.FirstOrDefault(p => p.X == x && p.Y == y);
+        //        if (y == floorY)
+        //        {
+        //            Console.Write('#');
+        //        }
+        //        else if (printer == null)
+        //        {
+        //            Console.Write('.');
+        //        }
+        //        else if (printer.Status == Status.Rock)
+        //        {
+        //            Console.Write('#');
+        //        }
+        //        else if (printer.Status == Status.Sand)
+        //        {
+        //            Console.Write('O');
+        //        }
+        //    }
+
+        //    Console.WriteLine();
+        //}
+    }
+
+    return numberOfSand;
+}
+
+HashSet<Point> GetStartingGrid(string[] lines)
+{
+    var grid = new HashSet<Point>();
+
+    foreach (var line in lines)
+    {
+        var rocks = line.Split(" -> ");
+        for (var i = 0; i < rocks.Length - 1; i++)
+        {
+            var fromRock = new Point(rocks[i], Status.Rock);
+            var toRock = new Point(rocks[i + 1], Status.Rock);
+
+            var instruction = fromRock.InstructionTo(toRock);
+            if (instruction is null)
+            {
+                continue;
+            }
+
+            if (instruction.Axis == Axis.X)
+            {
+                for (var x = fromRock.X; x != instruction.PointToStop; x += instruction.Direction)
+                {
+                    grid = Add(grid, new Point(x, fromRock.Y, Status.Rock));
+                }
+            }
+
+            if (instruction.Axis == Axis.Y)
+            {
+                for (var y = fromRock.Y; y != instruction.PointToStop; y += instruction.Direction)
+                {
+                    grid = Add(grid, new Point(fromRock.X, y, Status.Rock));
+                }
+            }
+        }
+    }
+
+    return grid;
+}
+
 HashSet<Point> Add(HashSet<Point> grid, Point point)
 {
-    if (!grid.Any(p => p.Equals(point)))
+    if (!grid.Any(p => p.EqualsPoint(point)))
     {
         grid.Add(point);
     }
@@ -143,7 +277,7 @@ record Point
         return null;
     }
 
-    public virtual bool Equals(Point point)
+    public bool EqualsPoint(Point point)
     {
         return X == point.X && Y == point.Y;
     }
