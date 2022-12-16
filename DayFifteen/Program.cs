026176a554d17bf034e773a4b92f4f38de09bde4
Kevin Lee <kevinlee1@corelogic.com>
2022-12-16 17:14:06 +0000

    WIP

--- DayFifteen/Program.cs
+++ DayFifteen/Program.cs
@@ -65,7 +65,36 @@ foreach (var line in lines)
 
 //PartOne(data, sensorsToBeacons, minimumX, maximumX);
 
-PartTwo(data, sensorsToBeacons);
+//PartTwo(data, sensorsToBeacons);
+
+var source = new CancellationTokenSource();
+var tasks = new List<Task>();
+const int max = 20;
+var start = DateTime.Now;
+for (var y = 0; y <= max + 1; y++)
+{
+    var t = y;
+    //tasks.Add(Task.Run(() =>
+    //{
+    //    var stop = PartTwoAgain(data, sensorsToBeacons, 0, max, t, start);
+    //    if (stop)
+    //    {
+    //        source.Cancel();
+    //    }
+    //}, source.Token));
+
+    var stop = PartTwoAgain(data, sensorsToBeacons, 0, max, t, start);
+    if (stop)
+    {
+        break;
+    }
+}
+
+try
+{
+    await Task.WhenAll(tasks);
+}
+catch { }
 
 void PartOne(Dictionary<int, Dictionary<int, Item?>> data, List<((int, int), (int, int), int)> sensorsToBeacons, int minimumX, int maximumX)
 {
@@ -76,8 +105,6 @@ void PartOne(Dictionary<int, Dictionary<int, Item?>> data, List<((int, int), (in
     {
         var sensorX = combinations.Item1.Item1;
         var sensorY = combinations.Item1.Item2;
-        var beaconX = combinations.Item2.Item1;
-        var beaconY = combinations.Item2.Item2;
         var sensorMaxRange = combinations.Item3;
 
         for (var x = minimumX; x <= maximumX; x++)
@@ -145,9 +172,10 @@ void PartTwo(Dictionary<int, Dictionary<int, Item?>> data, List<((int, int), (in
         {
             var xList = new HashSet<int>();
             var x = 0;
+            
             while (x <= max)
             {
-                Console.WriteLine(x);
+                //Console.WriteLine(x);
                 //if (x % 1000000 == 0)
                 //{
                 //    Console.WriteLine($"{index}: {x} || {minimumX} - {maximumX}");
@@ -169,21 +197,37 @@ void PartTwo(Dictionary<int, Dictionary<int, Item?>> data, List<((int, int), (in
                 if (delta > sensorMaxRange && !pointTaken && !confirmed.Contains((x, y)))
                 {
                     result.Add((x, y));
-                    //x++;
+                    x++;
                 }
                 else
                 {
                     result.Remove((x, y));
                     confirmed.Add((x, y));
 
-                    //var skip = (delta - y) * 2;
-                    //var amountMinus = sensorY - y;
-                    //var a = delta - amountMinus;
-                    //var skipFrom = sensorX - a;
+                    var skip = 0;
+                    
+                    if (sensorY == y)
+                    {
+                        skip = (delta * 2) + 2;
+                    }
+                    else if (y < sensorY)
+                    {
+                        skip = ((delta - sensorY + y) * 2) + 1;
+                    }
+                    else
+                    {
+                        skip = ((delta + sensorY - y) * 2) + 1;
+                    }
 
-                    //x = skipFrom + skip;
+                    var oldX = x;
+                    x += skip;
+
+                    for (var t = oldX; t <= x; t++)
+                    {
+                        result.Remove((t, y));
+                    }
                 }
-                x++;
+                //x++;
             }
 
             y++;
@@ -207,6 +251,242 @@ void PartTwo(Dictionary<int, Dictionary<int, Item?>> data, List<((int, int), (in
     //Print(data, minimumX, minimumY, maximumX, maximumY);
 }
 
+bool PartTwoAgain(Dictionary<int, Dictionary<int, Item?>> data, List<((int, int), (int, int), int)> sensorsToBeacons, int minimumX, int maximumX, int y, DateTime start)
+{
+    if (y % 10 == 0)
+    {
+        var now = DateTime.Now;
+        Console.WriteLine($"{now}: {y} {now - start}");
+        //Console.WriteLine(y);
+    }
+
+    //var now = DateTime.Now;
+    //Console.WriteLine($"{now}: {y} {now - start}");
+
+    var skips = new HashSet<(int, int)>();
+
+    var filledY = new HashSet<int>();
+    var index = 1;
+    foreach (var combinations in sensorsToBeacons)
+    {
+        var sensorX = combinations.Item1.Item1;
+        var sensorY = combinations.Item1.Item2;
+        var beaconX = combinations.Item2.Item1;
+        var beaconY = combinations.Item2.Item2;
+        var sensorMaxRange = combinations.Item3;
+
+        int? skipFrom = null;
+        int? skipTo = null;
+
+        var x = minimumX;
+        while (x <= maximumX)
+        {
+            if (skipFrom.HasValue && skipTo.HasValue && x >= skipFrom && x <= skipTo)
+            {
+                x = skipTo.Value + 1;
+            }
+
+            //if (x % 1000000 == 0)
+            //{
+            //    Console.WriteLine($"{index}: {x} || {minimumX} - {maximumX}");
+            //}
+
+            var xDelta = x - sensorX;
+            var yDelta = y - sensorY;
+            var delta = Math.Abs(xDelta) + Math.Abs(yDelta);
+
+            var pointTaken = false;
+            if (data.TryGetValue(x, out var xData))
+            {
+                if (xData.ContainsKey(y))
+                {
+                    pointTaken = true;
+                }
+            }
+
+            if (pointTaken && delta > sensorMaxRange)
+            {
+                filledY.Add(x);
+            }
+
+            if (delta > sensorMaxRange)
+            {
+                x++;
+            }
+
+            else if (delta <= sensorMaxRange)
+            {
+                //filledY.Add(x);
+
+                var skip = 0;
+                if (sensorY == y)
+                {
+                    skip = (delta * 2) + 2;
+                }
+                else if (y < sensorY)
+                {
+                    skip = ((delta - sensorY + y) * 2) + 1;
+                }
+                else
+                {
+                    skip = ((delta + sensorY - y) * 2) + 1;
+                }
+
+                var oldX = x;
+                x += skip;
+
+                skipFrom = !skipFrom.HasValue ? oldX : Math.Min(oldX, skipFrom.Value);
+                skipTo = !skipTo.HasValue ? x : Math.Max(x, skipTo.Value);
+                skipTo = skipTo > max ? max : skipTo;
+                skipTo--;
+            }
+
+            if (skipFrom.HasValue && skipTo.HasValue)
+            {
+                skips.Add((skipFrom.Value, skipTo.Value));
+            }
+        }
+
+        //Console.WriteLine($"{y} {index}");
+        index++;
+    }
+
+    //Console.WriteLine();
+
+    var distinctSkips = new HashSet<(int, int)>(new Comparer());
+    var skipsArray = skips.ToArray();
+    var queue = new Queue<(int, int)>();
+    foreach (var skip in skips)
+    {
+        queue.Enqueue(skip);
+    }
+
+    while (queue.TryDequeue(out var skip))
+    {
+        if (!distinctSkips.Any())
+        {
+            distinctSkips.Add(skip);
+        }
+        else
+        {
+            var temporarySkips = distinctSkips.ToHashSet(new Comparer());
+            foreach (var distinctSkip in distinctSkips)
+            {
+                (int?, int?) toSkip = (null, null);
+                if (skip.Item1 < distinctSkip.Item1 && skip.Item2 < distinctSkip.Item1)
+                {
+                    toSkip = skip;
+                }
+                else if (skip.Item1 < distinctSkip.Item1 && skip.Item2 == distinctSkip.Item1)
+                {
+                    temporarySkips.Remove(distinctSkip);
+                    toSkip = (skip.Item1, distinctSkip.Item2);
+                }
+                else if (skip.Item1 < distinctSkip.Item1 && skip.Item2 > distinctSkip.Item2)
+                {
+                    temporarySkips.Remove(distinctSkip);
+                    toSkip = skip;
+                }
+                else if (skip.Item1 == distinctSkip.Item1 && skip.Item2 > distinctSkip.Item2)
+                {
+                    temporarySkips.Remove(distinctSkip);
+                    toSkip = skip;
+                }
+                else if (skip.Item1 > distinctSkip.Item1 && skip.Item1 < distinctSkip.Item2 & skip.Item2 > distinctSkip.Item2)
+                {
+                    temporarySkips.Remove(distinctSkip);
+                    toSkip = (distinctSkip.Item1, skip.Item2);
+                }
+                else if (skip.Item1 < distinctSkip.Item1 && skip.Item2 > distinctSkip.Item1 && skip.Item2 < distinctSkip.Item2)
+                {
+                    temporarySkips.Remove(distinctSkip);
+                    toSkip = (skip.Item1, distinctSkip.Item2);
+                }
+                else if (skip.Item1 > distinctSkip.Item2)
+                {
+                    toSkip = skip;
+                }
+                else if (skip.Item1 == distinctSkip.Item2)
+                {
+                    temporarySkips.Remove(distinctSkip);
+                    toSkip = (distinctSkip.Item1, skip.Item2);
+                }
+                else if (skip.Item1 < distinctSkip.Item1 && skip.Item1 < distinctSkip.Item2 && skip.Item2 == distinctSkip.Item2)
+                {
+                    temporarySkips.Remove(distinctSkip);
+                    toSkip = skip;
+                }
+                else if (skip.Item1 >= distinctSkip.Item1 && skip.Item2 < distinctSkip.Item2)
+                {
+                    temporarySkips.Remove(skip);
+                    if (queue.Count == 1 && queue.Contains(skip))
+                    {
+                        queue.Clear();
+                    }
+                    break;
+                }
+                else if (skip.Item1 > distinctSkip.Item1 && skip.Item2 <= distinctSkip.Item2)
+                {
+                    temporarySkips.Remove(skip);
+                    if (queue.Count == 1 && queue.Contains(skip))
+                    {
+                        queue.Clear();
+                    }
+                    break;
+                }
+                else
+                {
+                    break;
+                }
+
+                if (toSkip.Item1.HasValue && toSkip.Item2.HasValue)
+                {
+                    var properSkip = (toSkip.Item1.Value, toSkip.Item2.Value);
+                    var addedToList = temporarySkips.Add(properSkip);
+                    if (toSkip.Item1 == minimumX && toSkip.Item2 == maximumX)
+                    {
+                        queue.Clear();
+                    }
+                    else if (addedToList)
+                    {
+                        queue.Enqueue(properSkip);
+                    }
+                }
+            }
+
+            distinctSkips = temporarySkips;
+        }
+    }
+
+    var finalFilled = new HashSet<int>();
+    foreach (var f in filledY)
+    {
+        foreach (var skip in distinctSkips)
+        {
+            if (f < skip.Item1 && f > skip.Item2)
+            {
+                finalFilled.Add(f);
+            }
+        }
+    }
+
+    var sum = distinctSkips.Sum(s => s.Item2 - s.Item1);
+    sum = sum > maximumX ? maximumX : sum;
+    var filled = finalFilled.Count + sum;
+    var answer = maximumX - filled;
+    if (answer == 1)
+    {
+        var range = new HashSet<int>(Enumerable.Range(minimumX, maximumX));
+        range.ExceptWith(filledY);
+        Console.WriteLine($"Answer: {range.First()}, {y}");
+
+        return true;
+    }
+
+    return false;
+
+    //Print(data, minimumX, minimumY, maximumX, maximumY);
+}
 static void Print(Dictionary<int, Dictionary<int, Item?>> grid, int fromX, int fromY, int toX, int toY)
 {
     for (var y = fromY - 10; y <= toY + 10; y++)
